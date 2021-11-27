f796e9063935997f91e869c82ec6a4d20e31b26c
Din Bibek Thapa <legendsnever7die@gmail.com>
2021-11-27 15:57:07 +0545

    express session added part 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -29,13 +31,21 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-12345-09876-54321'));
+// app.use(cookieParser('12345-67890-12345-09876-54321'));
+
+app.use(session({
+  name:'hamroSession',
+  secret: '12345-67890-12345-09876-54321',
+  saveUninitialized: false,
+  store: new FileStore(),
+  resave: false,
+}));
 
 function auth(req, res, next) {
 
-  console.log(req.signedCookies)
+  console.log(req.session)
 
-  if(!req.signedCookies.user) {
+  if(!req.session.user) {
 
     var authHeader = req.headers.authorization
 
@@ -51,7 +61,7 @@ function auth(req, res, next) {
     var password = auth[1]
   
     if( username === 'admin' && password === 'admin123' ) {
-      res.cookie('user', 'admin', { signed: true })
+      req.session.user = 'admin'
       next()
     } else {
       var err = new Error('You are not authenticated!')
@@ -60,7 +70,7 @@ function auth(req, res, next) {
       return next(err)
     }
   } else {
-    if(req.signedCookies.user === 'admin') {
+    if(req.session.user === 'admin') {
       next()
     } else{
       var err = new Error('You are not authenticated!')
